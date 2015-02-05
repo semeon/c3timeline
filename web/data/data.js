74bdca8a97aa16c0c15085c33dee1d4d020a81a6
semeon <semeon.peradze@gmail.com>
2015-02-05 15:30:29 -0800

    two series of static data working

--- web/data/data.js
+++ web/data/data.js
@@ -1,11 +1,73 @@
 var myData = {
 	project: 'Ratchet',
 	dates: [
-		{date: '2015-02-11', priority: 1, event: 'Internal Demo'},
-		{date: '2015-02-16', priority: 2, event: 'Beta Release'},
-		{date: '2015-03-13', priority: 3, event: 'Alpha Release'},
-		{date: '2015-03-31', priority: 4, event: 'Production Release'},
-		{date: '2015-04-10', priority: 2, event: 'Patch #1'},
-		{date: '2015-04-20', priority: 1, event: 'Patch #2'}
+		{date: '2015-02-11', priority: 2, event: 'Int. Demo'},
+		{date: '2015-02-16', priority: 2, event: 'Alpha Demo'},
+		{date: '2015-03-13', priority: 2, event: 'Beta Release'},
+		{date: '2015-03-31', priority: 2, event: 'Production Release'},
+		{date: '2015-04-20', priority: 2, event: 'Patch #2'}
 	]
 };
+
+var dataSeries4 = {
+
+	columns: [
+		['x', 	
+			'2015-02-04', 	
+			'2015-02-11', 	
+			'2015-02-16', 	
+			'2015-03-13'],
+
+		['Ratchet',
+			null, 
+			2, 
+			2, 
+			2],
+
+		['MPSD',
+			1, 
+			1, 
+			null, 
+			null]
+	],
+
+	// labels['Ratchet']: 
+	// [
+	// 	null,	
+	// 	'Int. Demo',
+	// 	'Alpha Demo',
+	// 	'Beta Release'],
+
+	// labels['MPSD']: 
+	// [
+	// 	'IA Review',
+	// 	'Hi-Fi Desing',
+	// 	null,
+	// 	null]
+	// ]
+};
+
+
+var labels = {};
+labels['Ratchet'] =  
+	[
+		null,	
+		'Int. Demo',
+		'Alpha Demo',
+		'Beta Release'
+	];
+
+labels['MPSD'] =
+	[
+		'IA Review',
+		'Hi-Fi Desing',
+		null,
+		null
+	];
+
+
+
+
+
+
+
--- web/js/c3code.js
+++ web/js/c3code.js
@@ -24,12 +24,44 @@ var genColumns = function(d) {
 
 }
 
+var genColumns2 = function(d) {
+    // columns: [
+    //     ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+    //     ['data1', 30, 200, 100, 400, 150, 250],
+    //     ['data2', 130, 340, 200, 500, 250, 350]
+    // ]
+
+	var result = [];
+
+	var xAxisArray = [];
+
+	xAxisArray.push('x');
+
+	var dataRow = [];
+	dataRow.push(d.project);
+
+	for (var i = 0; i<d.dates.length; i++) {
+		var date = d.dates[i].date;
+		var pr = d.dates[i].priority;
+		xAxisArray.push(date);
+		dataRow.push(pr);
+	};
+
+	result.push(xAxisArray);
+	result.push(dataRow);
+
+	console.log("X: ");
+	console.log(result);
+	return result;
+
+}
+
 
 var chart = c3.generate({
     data: {
         x: 'x',
         //xFormat: '%Y-%m-%d-%w', // 'xFormat' can be used as custom format of 'x'
-        columns: genColumns(myData),
+        columns: dataSeries4.columns,
 	    type: 'bar',
 	    labels: {
 	    	format: function (v, id, i, j) { 
@@ -43,7 +75,8 @@ var chart = c3.generate({
 					// console.log("-- i: " +  i + " ----------------");
 					// console.log(myData.dates[i]);
 					// console.log("------------------------");
-					result = myData.dates[i].event;
+					result = labels[id][i];
+
 				};
 	   			return result; 	    		
 	    	}
@@ -52,11 +85,11 @@ var chart = c3.generate({
 
 
     axis: {
-        x: {type: 'timeseries', tick: { format: '%m/%d' }},
+        x: {type: 'timeseries', tick: { format: '%m/%d', fit: false, centered: false }},
 		y: {
 			show: false,
 			min: 0,
-			max: 10,
+			max: 3,
 			padding: {
 				top: 0,
 				bottom: 0
@@ -64,7 +97,7 @@ var chart = c3.generate({
 		}
     },
 
-	bar: { zerobased: true, width: 15},
+	bar: { zerobased: true, width: 10},
 	grid: { x: {show: true}	},
 	size: {height: 200},
 
@@ -75,22 +108,12 @@ var chart = c3.generate({
 				return name; 
 			},
 			value: function (value, ratio, id, index) { 
-				if (index != undefined) {
-					return myData.dates[index].event;
+				if (index != undefined ) {
+					return labels[id][index];
+					// return labels[id][i];
 				}
 			}
 		}
-
 	}	
-
-
-
 });
 
-// setTimeout(function () {
-//     chart.load({
-//         columns: [
-//             ['data3', 400, 500, 450, 700, 600, 500]
-//         ]
-//     });
-// }, 1000);
